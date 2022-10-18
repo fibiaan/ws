9ed204948c94805bb06c51bac98685c23c68c864
fibiaan <fibianmejia@hotmail.com>
2022-10-18 15:25:34 -0500

    latest

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,32 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = require("fs");
 const app = (0, express_1.default)();
-const port = 8011;
+const port = 8001;
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
     console.log('fibiaan');
     res.send('hey');
 });
+app.post('/payload', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log('payload');
+    yield (0, fs_1.writeFileSync)('query.json', JSON.stringify(req.body));
+    console.log(req.body);
+    res.end();
+}));
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
-import express, {Express, Request, Response} from 'express'
+import express, { Express, Request, Response } from 'express'
+import { writeFileSync } from 'fs'
 
 const app: Express = express()
-const port = 8011
+const port = 8001
+
+app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
     console.log('fibiaan')
     res.send('hey')
+})
+
+app.post('/payload', async (req: Request, res: Response) => {
+    console.log('payload')
+    await writeFileSync('query.json', JSON.stringify(req.body))
+    console.log(req.body)
+    res.end()
 })
 
 app.listen(port, () => {
